386c246489eb25ff1ff90bd2d0ff3f4378862639
Javier Suárez <javiersuarsan@gmail.com>
2022-06-05 13:42:16 +0200

    feat - add keyvalue component portfolio page

---
+++ src/app/demo-ngx-keyvalue/demo-ngx-keyvalue.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from '@suarsan/ngx-keyvalue/lib/item.interface';
+
+@Component({
+  selector: 'app-demo-ngx-keyvalue',
+  templateUrl: './demo-ngx-keyvalue.component.html',
+  styleUrls: ['./demo-ngx-keyvalue.component.scss']
+})
+export class DemoNgxKeyvalueComponent implements OnInit {
+
+  input: Array<Item> = [
+    { key: 'key_1', value: 'value_1' },
+    { key: 'key_2', value: 'value_2' },
+    { key: 'key_3', value: 'value_3' },
+    { key: 'key_4', value: 'value_4' },
+    { key: 'key_5', value: 'value_5' }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
