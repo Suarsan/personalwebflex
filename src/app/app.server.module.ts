6ecbf4f95649c70f7139084b0dee007a0950f6e1
Javier Suárez <javiersuarsan@gmail.com>
2022-06-05 12:15:03 +0200

    perf - angular universal server translations

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { translateServerLoaderFactory } from './loaders/translate-server.loader';
+import { TransferState } from '@angular/platform-browser';
 
 @NgModule({
   imports: [
     AppModule,
-    ServerModule
+    ServerModule,
+    ServerTransferStateModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateServerLoaderFactory,
+        deps: [TransferState]
+      }
+    })
   ],
   bootstrap: [AppComponent]
 })
