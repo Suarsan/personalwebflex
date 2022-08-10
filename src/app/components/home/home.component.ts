c8a17a67b920f5d7817320dcc248c3af44a234a7
Javier Suárez <javiersuarsan@gmail.com>
2022-08-11 00:11:43 +0200

    refactor - change javascript layout to vh

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,39 +1,12 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  window;
-  screenHeight: number;
-  topSpace: number;
-
-  constructor(@Inject(DOCUMENT) private document) {
-    this.window = this.document.defaultView;
- }
-
-  ngOnInit() {
-    this.layout();
-  }
-
-  @HostListener('window:resize', ['event'])
-  onResize() {
-    if (this.window.innerWidth > 768) {
-      this.layout();
-    }
-  }
-  
-  private layout() {
-    this.screenHeight = this.window.innerHeight;
-    if (this.screenHeight < 768) {
-      this.topSpace = this.screenHeight * 0.2;
-    } else {
-      this.topSpace = this.screenHeight * 0.3;
-    }
-  }
+  constructor() {}
 
 }
