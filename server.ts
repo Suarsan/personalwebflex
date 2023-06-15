4d27eadaf6c478903638d99d9b07bc13a02e8db8
Javier Suárez <javiersuarsan@gmail.com>
2023-06-14 20:12:22 -0400

    [fix] some deployment settings

--- server.ts
+++ server.ts
@@ -1,12 +1,11 @@
 import 'zone.js/dist/zone-node';
-
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
 import { join } from 'path';
-
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
+import 'dotenv/config'
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -38,7 +37,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'];
 
   // Start up the Node server
   const server = app();
--- src/app/loaders/translate-server.loader.ts
+++ src/app/loaders/translate-server.loader.ts
@@ -1,44 +1,30 @@
 import { join } from 'path';
 import { Observable } from 'rxjs';
 import { TranslateLoader } from '@ngx-translate/core';
-import {
-  makeStateKey,
-  StateKey,
-  TransferState
-} from '@angular/platform-browser';
+import { makeStateKey, StateKey, TransferState } from '@angular/platform-browser';
 import * as fs from 'fs';
 
 export class TranslateServerLoader implements TranslateLoader {
-  constructor(
-    private transferState: TransferState,
-    private prefix: string = 'i18n',
-    private suffix: string = '.json'
-  ) {}
+  
+  constructor(private transferState: TransferState) { }
 
   public getTranslation(lang: string): Observable<any> {
     return new Observable((observer) => {
-      const assets_folder = join(
-        process.cwd(),
-        'dist',
-        'personalwebflex',
-        'browser',
-        'assets',
-        this.prefix
-      );
 
-      const jsonData = JSON.parse(
-        fs.readFileSync(`${assets_folder}/${lang}${this.suffix}`, 'utf8')
-      );
+      const assets_folder = join(process.cwd(), 'browser', 'assets', 'i18n');
 
-      const key: StateKey<number> = makeStateKey<number>(
-        'transfer-translate-' + lang
-      );
+      const jsonData = JSON.parse(fs.readFileSync(`${assets_folder}/${lang}.json`, 'utf8'));
+
+      const key: StateKey<number> = makeStateKey<number>('transfer-translate-' + lang);
+
       this.transferState.set(key, jsonData);
 
       observer.next(jsonData);
       observer.complete();
+
     });
   }
+
 }
 
 export function translateServerLoaderFactory(transferState: TransferState) {
